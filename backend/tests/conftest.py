e8411b46f6dc82dc9715a8ad82f4add997ad89da
PhT09 <trannguyentoanphat1592005@gmail.com>
2026-03-28 09:12:22 +0700

    test: hoàn thành unit test cho CRUD của subject

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -11,7 +12,9 @@ from app.models.base import Base
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
