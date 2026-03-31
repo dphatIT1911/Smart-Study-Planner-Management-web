1fea5940a677bd73f5d4bb7ecddff3443b69c603
dphatIT1911 <vphat545@gmail.com>
2026-04-01 00:47:27 +0700

    fix: removed alembic from start.sh since db uses Base.metadata.create_all

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -3,8 +3,5 @@
 # Thoat ngay neu co loi
 set -e
 
-echo "--- Executing Database Migrations ---"
-alembic upgrade head
-
 echo "--- Starting Gunicorn Server ---"
 exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker app.main:app --bind 0.0.0.0:${PORT:-8000}
