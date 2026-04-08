4bccddd5b83b6ad85a8934c0e43e0dc2d89c4b2d
PhT09 <trannguyentoanphat1592005@gmail.com>
2026-04-08 18:33:07 +0700

    Cấu hình API_URL cho frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const config = {
   API_BASE_URL:
     import.meta.env.DEV
       ? import.meta.env.VITE_API_URL
-      : 'https://study-planner-deploy.onrender.com',
+      : 'https://study-planner-deploy-server-api.onrender.com',
 };
 
 export default config;
