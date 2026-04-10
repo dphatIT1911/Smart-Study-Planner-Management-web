3afbca864b4115fc54c19592eb26afd4da888bf5
PhT09 <trannguyentoanphat1592005@gmail.com>
2026-04-10 20:35:45 +0700

    Cập nhật logic biến môi trường cho BE và FE

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,9 +2,9 @@ const config = {
   IS_PROD: import.meta.env.PROD,
   IS_DEV: import.meta.env.DEV,
   API_BASE_URL:
-    import.meta.env.DEV
+    import.meta.env.PROD
       ? import.meta.env.VITE_API_URL
-      : 'https://abc.com',
+      : 'http://localhost:8000',
 };
 
 export default config;
